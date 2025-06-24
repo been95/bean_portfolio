c8042196e5d994e8bf4f2d8a24fb1e74e86073cd
been95 <dcb95@naver.com>
2025-06-24 18:50:25 +0900

    js

--- js/common.js
+++ js/common.js
@@ -17,3 +17,47 @@ arrowImg.addEventListener("click", () => {
     // behavior: "smooth",
   });
 });
+
+const buttons = document.querySelectorAll(".work button");
+buttons.forEach((button) => {
+  button.addEventListener("mouseenter", (e) => {
+    const img = e.currentTarget.querySelector("img");
+    img.src = img.src.replace(".jpg", ".gif");
+  });
+  button.addEventListener("mouseleave", (e) => {
+    const img = e.currentTarget.querySelector("img");
+    img.src = img.src.replace(".gif", ".jpg");
+  });
+});
+
+/*
+const btn = document.querySelector(".been");
+btn.addEventListener("mouseenter", (e) => {
+  const img = e.currentTarget.children[0];
+  img.setAttribute("src", "./images/been.gif");
+});
+btn.addEventListener("mouseleave", (e) => {
+  const img = e.currentTarget.children[0];
+  img.setAttribute("src", "./images/been.jpg");
+});
+
+const star = document.querySelector(".star");
+star.addEventListener("mouseenter", (e) => {
+  const img = e.currentTarget.children[0];
+  img.setAttribute("src", "./images/star.gif");
+});
+star.addEventListener("mouseleave", (e) => {
+  const img = e.currentTarget.children[0];
+  img.setAttribute("src", "./images/star.jpg");
+});
+
+const painting = document.querySelector(".painting");
+painting.addEventListener("mouseenter", (e) => {
+  const img = e.currentTarget.children[0];
+  img.setAttribute("src", "./images/painting.gif");
+});
+painting.addEventListener("mouseleave", (e) => {
+  const img = e.currentTarget.children[0];
+  img.setAttribute("src", "./images/painting.jpg");
+});
+*/
